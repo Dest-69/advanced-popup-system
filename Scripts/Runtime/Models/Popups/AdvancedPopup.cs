0c8ae2bb297cf0d8c3d89f0d46e3ef04d48b12e8
Vitaliy <vetalpopovetskiy@gmail.com>
2022-11-10 23:49:35 +0400

    add require

--- Scripts/Runtime/Models/Popups/AdvancedPopup.cs
+++ Scripts/Runtime/Models/Popups/AdvancedPopup.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 
+[RequireComponent(typeof(CanvasGroup))]
 public class AdvancedPopup : IAdvancedPopup
 {
     public CanvasGroup _canvasGroup;
